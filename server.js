5e7d15c5e1d75c4ebef2d112868a153da576886e
Divyansh undlet <116184514+Dishu12341234@users.noreply.github.com>
2023-09-03 13:47:19 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   .DS_Store
    	modified:   server.js
    	new file:   sql.SQL
    	new file:   views/add.js
    	new file:   views/add.pug
    	new file:   views/home.css
    	modified:   views/index.pug

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require("express")
 const app = express()
 const fs = require("fs")
+const mysql = require("mysql")
 
 app.set("view engine","pug")
+app.use(express.static("views"))
+
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "divyansh",
+    password: "divyansh@mysql"
+  });
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  
 
 function home(req,res)
 {
-    res.render("index")
+    con.query("SHOW DATABASES",(e,r)=>{
+        console.log(r,e);
+        res.render("index")
+    })
 }
 
+function add_user(req,res)
+{
+    res.render("add")
+}
 app.get("/",home)
+app.get("/add_user",add_user)
 
 app.listen(8080)
