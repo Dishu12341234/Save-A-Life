8801c46a2a368fb00a182bf430fec823eb0b5437
Divyansh undlet <116184514+Dishu12341234@users.noreply.github.com>
2023-09-09 16:58:54 +0530

    all

--- views/index.js
+++ views/index.js
@@ -1,29 +1,40 @@
 const patients = fetch(`${window.location.origin}/get_patients`)
 const table = document.createElement('table')
-table.border= '1'
-patients.then(r=>{
-    r.json().then((r)=>{display(r);THs(r)})
+table.border = '1'
+patients.then(r => {
+    r.json().then((r) => { THs(r);display(r) })
 })
 
 function THs(r) {
+    let count = 0
+    let tr = document.createElement('tr')
+    for (let i in r[0]) {
+        const th = document.createElement("th")
+        if(i === 'login')
+        continue;
+        if(i != "Blood_group")
+        th.innerText = i
+        else
+        th.innerText = "Blood Group"
+        tr.appendChild(th)
+        ++count;
+    }
+    table.appendChild(tr)
 
 }
-
-function display(r)
-{
-    for(let k in r)
-    {
+function display(r) {
+    for (let k in r) {
         let tr = document.createElement('tr')
-        
-        for(let l in r[k])
-        {
-            const td = document.createElement('td')
-            td.innerText = r[k][l] 
-            if(l != 'login')
-            tr.appendChild(td)
+        for (let l in r[k]) {
+            if (l !== 'login') {
+                const td = document.createElement('td')
+                td.innerText = r[k][l]
+                tr.appendChild(td)
+            }
         }
-        table.appendChild(tr)
+        table.appendChild(tr) // Append each row to the table individually
     }
 }
+
 
 document.body.appendChild(table)
